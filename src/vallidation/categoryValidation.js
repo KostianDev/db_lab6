dfbbb4fef5d23de74f22293cf8ae0b75aaa11726
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2024-12-12 17:10:56 +0200

    add validation

---
+++ src/vallidation/categoryValidation.js
@@ -0,0 +1,7 @@
+export const validateCategory = (category) => {
+    const {name} = category;
+
+    if (!name) {
+        throw new Error('Missing required field: name');
+    }
+};
---
+++ src/vallidation/dataValidation.js
@@ -0,0 +1,15 @@
+export const validateData = (data) => {
+    const requiredFields = [
+        'name',
+        'description',
+        'format',
+        'content',
+        'createdAt'
+    ];
+
+    for (const field of requiredFields) {
+        if (!data[field]) {
+            throw new Error(`Missing required field: ${field}`);
+        }
+    }
+};
